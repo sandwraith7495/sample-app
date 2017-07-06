ee527226857f8ad24fa95dabd72536b80d72d0ac
sandwraith8 <sandwraith7495@gmail.com>
2017-07-06 14:52:47 +0700

    chapter 3-4 fixed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def full_title page_title = ""
+    base_title = t "title"
+
+    return base_title if page_title.empty?
+    page_title + " | " + base_title
+  end
 end
