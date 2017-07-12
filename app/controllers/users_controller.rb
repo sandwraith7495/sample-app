00031fe967f61de83e29612f8d97a1414af326cd
sandwraith8 <sandwraith7495@gmail.com>
2017-07-12 15:52:45 +0700

    user sign up fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,32 @@
 class UsersController < ApplicationController
-  def new; end
+  attr_reader :user
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+
+    if user.save
+      flash[:success] = t "welcome"
+      redirect_to user
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = user = User.find_by id: params[:id]
+
+    return if user.present?
+    flash[:danger] = t "danger"
+    redirect_to root_url
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation
+  end
 end
